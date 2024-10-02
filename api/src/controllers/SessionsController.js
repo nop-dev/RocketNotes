06f4ea31fc0395fda4092d07372b9cb38a57154e
Yuri Sousa <ysousa.dev@gmail.com>
2024-10-02 18:05:36 -0300

    feat: :sparkles: adicicona validação de usuário por email

--- api/src/controllers/SessionsController.js
+++ api/src/controllers/SessionsController.js
@@ -1,8 +1,18 @@
+const knex = require("../database/knex");
+const AppError = require("../utils/AppError");
+
 class SessionsController {
 	async create(req, res) {
 		const { email, password } = req.body;
 
-		return res.json({ email, password })
+		const user = await knex("users").where({ email }).first();
+
+		if(!user) {
+			throw new AppError("E-mail ou senha incorreta... Tente novamente.", 401);
+			
+		}
+
+		return res.json(user)
 	}
 }
 
