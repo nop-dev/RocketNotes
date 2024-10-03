a8b1dc72803a4c7ff883430bdf9a8274406c12a1
Yuri Sousa <ysousa.dev@gmail.com>
2024-10-02 21:07:36 -0300

    feat: :sparkles: adiciona jwt, adiciona lógica da criação do token

--- api/src/controllers/SessionsController.js
+++ api/src/controllers/SessionsController.js
@@ -1,6 +1,9 @@
 const knex = require("../database/knex");
 const AppError = require("../utils/AppError");
-const { compare } = require("bcryptjs")
+const authConfig = require("../configs/auth");
+const { sign } = require("jsonwebtoken");
+
+const { compare } = require("bcryptjs");
 
 class SessionsController {
 	async create(req, res) {
@@ -8,17 +11,23 @@ class SessionsController {
 
 		const user = await knex("users").where({ email }).first();
 
-		if(!user) {
+		if (!user) {
 			throw new AppError("E-mail ou senha incorreta... Tente novamente.", 401);
 		}
 
-		const passwordCheck = await compare(password, user.password)
+		const passwordCheck = await compare(password, user.password);
 
-		if(!passwordCheck) {
-			throw new AppError("E-mail ou senha incorreta... Tente novamente.", 401)
+		if (!passwordCheck) {
+			throw new AppError("E-mail ou senha incorreta... Tente novamente.", 401);
 		}
 
-		return res.json(user)
+		const { secret, expiresIn } = authConfig.jwt;
+		const token = sign({}, secret, {
+			subject: String(user.id),
+			expiresIn,
+		});
+
+		return res.status(200).json({ user, token });
 	}
 }
 
