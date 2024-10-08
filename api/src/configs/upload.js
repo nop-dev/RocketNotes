d05b141f0ba5fb3834969dee5e3f31241ed16223
Yuri Sousa <ysousa.dev@gmail.com>
2024-10-08 20:36:32 -0300

    feat: :sparkles: cria forma de carregar o avatar (sem db)

--- api/src/configs/upload.js
+++ api/src/configs/upload.js
@@ -3,13 +3,13 @@ const multer = require("multer");
 const crypto = require("node:crypto");
 
 const TMP_FOLDER = path.resolve(__dirname, "..", "..", "tmp");
-const UPLOADS_FOLDER = path.resolve(__dirname, "uploads");
+const UPLOADS_FOLDER = path.resolve(TMP_FOLDER, "uploads");
 
 const MULTER = {
 	storage: multer.diskStorage({
 		destination: TMP_FOLDER,
 		filename(request, file, callback) {
-			const fileHash = crypto.ramdomBytes(10).toString("hex");
+			const fileHash = crypto.randomBytes(10).toString("hex");
 			const fileName = `${fileHash}-${file.originalname}`;
 
 			return callback(null, fileName);
--- api/src/routes/users.routes.js
+++ api/src/routes/users.routes.js
@@ -1,9 +1,12 @@
-const { Router } = require("express");
+const { Router, response } = require("express");
+const multer = require("multer");
+const uploadConfig = require("../configs/upload");
 
 const UsersController = require("../controllers/UsersController");
 const ensureAuth = require("../middleware/ensureAuth");
 
 const usersRoutes = Router();
+const upload = multer(uploadConfig.MULTER);
 
 /* Função feita em aula para compreensão de conceito, mas que acabou não sendo implementada na versão final...
 
@@ -21,5 +24,14 @@ const usersController = new UsersController();
 
 usersRoutes.post("/", usersController.create);
 usersRoutes.put("/", ensureAuth, usersController.update);
+usersRoutes.patch(
+	"/avatar",
+	ensureAuth,
+	upload.single("avatar"),
+	(req, res) => {
+		console.log(req.file.filename);
+		res.json();
+	},
+);
 
 module.exports = usersRoutes;
