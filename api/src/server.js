7ddb45ca3653999a076229369ff485790f7b0986
Yuri Sousa <ysousa.dev@gmail.com>
2024-10-08 22:29:12 -0300

    feat: :heavy_plus_sign: adiciona cors

--- api/src/server.js
+++ api/src/server.js
@@ -1,35 +1,37 @@
-require('express-async-errors');
-const migrationsRun = require("./database/sqlite/migrations")
-const AppError = require('./utils/AppError');
+require("express-async-errors");
+const migrationsRun = require("./database/sqlite/migrations");
+const AppError = require("./utils/AppError");
+const cors = require("cors");
 
-const uploadConfig = require("./configs/upload")
+const uploadConfig = require("./configs/upload");
 
 const express = require("express");
 
 const routes = require("./routes");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
-app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER))
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
 
 app.use(routes);
 migrationsRun();
 
-app.use(( error, request, response, next ) => {
- if(error instanceof AppError) {
-    return response.status(error.statusCode).json({
-        status: "error",
-        message: error.message
-    });
- };
+app.use((error, request, response, next) => {
+	if (error instanceof AppError) {
+		return response.status(error.statusCode).json({
+			status: "error",
+			message: error.message,
+		});
+	}
 
- console.error(error);
+	console.error(error);
 
- return response.status(500).json({
-    status: "error",
-    message: "Internal server error"
- });
+	return response.status(500).json({
+		status: "error",
+		message: "Internal server error",
+	});
 });
 
 const PORT = 3000;
