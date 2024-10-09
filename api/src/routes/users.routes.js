c21cf9ae1efcaf4148b60eb004e52f7922c59393
Yuri Sousa <ysousa.dev@gmail.com>
2024-10-08 21:22:04 -0300

    feat: :sparkles: adiciona forma de buscar avatar cadastrado, adiciona rota /files

--- api/src/routes/users.routes.js
+++ api/src/routes/users.routes.js
@@ -3,6 +3,7 @@ const multer = require("multer");
 const uploadConfig = require("../configs/upload");
 
 const UsersController = require("../controllers/UsersController");
+const UserAvatarController = require("../controllers/UserAvatarController");
 const ensureAuth = require("../middleware/ensureAuth");
 
 const usersRoutes = Router();
@@ -21,17 +22,10 @@ function myMiddleware(request, response, next) {
 }; */
 
 const usersController = new UsersController();
+const userAvatarController = new UserAvatarController();
 
 usersRoutes.post("/", usersController.create);
 usersRoutes.put("/", ensureAuth, usersController.update);
-usersRoutes.patch(
-	"/avatar",
-	ensureAuth,
-	upload.single("avatar"),
-	(req, res) => {
-		console.log(req.file.filename);
-		res.json();
-	},
-);
+usersRoutes.patch("/avatar", ensureAuth, upload.single("avatar"), userAvatarController.update);
 
 module.exports = usersRoutes;
