9a9ee3147c2b46c9606e32a010c042aa411a2962
Yuri Sousa <ysousa.dev@gmail.com>
2025-03-11 09:37:35 -0300

    feat: :fire: adiciona um UserRepository pra separar a lógica do banco da aplicação

--- api/src/controllers/UsersController.js
+++ api/src/controllers/UsersController.js
@@ -1,7 +1,6 @@
 const { hash, compare } = require("bcryptjs");
 const AppError = require("../utils/AppError");
 
-const sqliteConnection = require("../database/sqlite");
 class UsersController {
 	async create(request, response) {
 		const { name, email, password } = request.body;
@@ -9,22 +8,14 @@ class UsersController {
 		if (!name) {
 			throw new AppError("O nome é obrigatório...");
 		}
-
-		const database = await sqliteConnection();
-		const checkIfUserExists = await database.get(
-			"SELECT * FROM users WHERE email = (?)",
-			[email],
-		);
+	
 		if (checkIfUserExists) {
 			throw new AppError("Este e-mail já está em uso...");
 		}
 
 		const hashedPassword = await hash(password, 8);
 
-		await database.run(
-			"INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
-			[name, email, hashedPassword],
-		);
+		
 
 		response.status(201).json();
 
